a6e2faf93bfb501265bd138f6ad9456fa31a742f
Chris Perrins <chrisperrins95@gmail.com>
2020-05-27 14:49:40 +0100

    003-evenly-spaced-added

---
+++ 003-evenly-spaced/README.md
@@ -0,0 +1,11 @@
+# Evenly Spaced
+
+## Overview
+The method takes in three integers, you need to write out a method which 
+determines whether the inputs are evenly spaced.
+
+There are multiple ways to do this:
+- A series of if statements
+- A sort algorithm can be applied
+- Many others...
+
---
+++ 003-evenly-spaced/src/main/java/com/qa/portfolio/EvenlySpaced.java
@@ -0,0 +1,18 @@
+package com.qa.portfolio;
+
+public class EvenlySpaced {
+
+	/**
+	 * Returns true if the three values are evenly spaced <br>
+	 * Works with any order
+	 * 
+	 * @param num1 integer value
+	 * @param num2 integer value
+	 * @param num3 integer value
+	 * @return boolean value for if the 3 values entered are evenly spaced
+	 */
+	public boolean evenlySpaced(int num1, int num2, int num3) {
+		//TODO
+		return false;
+	}
+}

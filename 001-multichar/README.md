49a269620c7853c784d1d0bcc2b8ff4b4b5d9b9d
Chris Perrins <chrisperrins95@gmail.com>
2020-05-27 11:29:02 +0100

    001-multichar added

---
+++ 001-multichar/README.md
@@ -0,0 +1,5 @@
+# MultiChar Task
+
+## Overview
+For a given input, triplicate each character and then concatenate all 
+the characters into a single string output
---
+++ 001-multichar/src/main/java/com/qa/portfolio/MultiChar.java
@@ -0,0 +1,18 @@
+package com.qa.portfolio;
+
+
+public class MultiChar {
+
+	/**
+	 * For each character in the entered string the method will triplicate
+	 * each the character and return the concatenated string.
+	 * 
+	 * @param input - any string
+	 * @return triplicates of each letter from the input
+	 */
+	public String multiChar(String input) {
+		// TODO
+		return "";
+	}
+
+}

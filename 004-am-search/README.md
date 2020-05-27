541ebfb4198924c1bf276c7a137076290a3190d6
Chris Perrins <chrisperrins95@gmail.com>
2020-05-27 15:35:32 +0100

    004-am-search added

---
+++ 004-am-search/README.md
@@ -0,0 +1,6 @@
+# Am Search
+
+## Overview
+Search for the number of occurances of the word "am" in a sentence.
+
+For instance "I **_am_** in Amsterdam I **_am_**" has only two occurances. 
---
+++ 004-am-search/src/main/java/com/qa/portfolio/AmSearch.java
@@ -0,0 +1,15 @@
+package com.qa.portfolio;
+
+public class AmSearch {
+
+	/**
+	 * Returns the number of times "am" appears in a String ignoring case <br>
+	 * Only counts "am" if it is not proceeded or followed by another character 
+	 * 
+	 * @param sentence to search for "am" in
+	 * @return the count of "am" words
+	 */
+	public int amSearch(String sentence) {
+		return -1;
+	}
+}
